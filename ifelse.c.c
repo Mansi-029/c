4041644c314d82c0926ad5d9cb7e3b535d21c6f1
MansiSingh2 <117916600+MansiSingh2@users.noreply.github.com>
2023-05-07 15:07:15 +0530

    Add files via upload

---
+++ ifelse.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	char ch;
+	printf("Enter any character: \n");
+	scanf("%c",&ch);
+	
+	if(ch >= 'A' && ch <= 'Z')
+	    printf("Upper case\n");
+	else if(ch >= 'a' && ch <= 'z')
+	    printf("Lower case\n");
+	else
+	    printf("Specal character\n");
+	return 0;
+}
